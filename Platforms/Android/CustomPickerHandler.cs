b3be79ab5c7f3eef414f9665a0a0c4808bfd7b44
ZeroXRagde <ragde360@gmail.com>
2025-11-20 00:22:28 -0600

    Create CustomPickerHandler.cs

---
+++ Platforms/Android/CustomPickerHandler.cs
@@ -0,0 +1,27 @@
+﻿using Android.Graphics.Drawables;
+using Microsoft.Maui.Handlers;
+
+namespace PkHexA.Platforms.Android
+{
+    public class CustomPickerHandler : PickerHandler
+    {
+        protected override void ConnectHandler(global::Android.Widget.EditText platformView)
+        {
+            base.ConnectHandler(platformView);
+
+            // Fondo oscuro tipo PKHeX
+            platformView.SetBackgroundColor(Color.ParseColor("#202020"));
+
+            // Texto blanco
+            platformView.SetTextColor(Color.White);
+
+            // Quitar borde feo de Android
+            platformView.BackgroundTintList =
+                Android.Content.Res.ColorStateList.ValueOf(Color.Transparent);
+
+            // Popup (Spinner)
+            if (platformView.Background is GradientDrawable gd)
+                gd.SetColor(Color.ParseColor("#202020"));
+        }
+    }
+}
